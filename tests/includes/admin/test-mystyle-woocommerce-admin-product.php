2386512d6f5c89c4f01b8c73c3105b1674cc6d3e
Collin Krawll <collin.krawll@thirdset.com>
2015-06-25 17:24:28 -0700

    Added PHPUnit testing for the MyStyle_WooCommerce_Admin_product::process_mystyle_data_panel function

--- tests/includes/admin/test-mystyle-woocommerce-admin-product.php
+++ tests/includes/admin/test-mystyle-woocommerce-admin-product.php
@@ -84,8 +84,42 @@ class MyStyleWooCommerceAdminProductTest extends WP_UnitTestCase {
     /**
      * Test the process_mystyle_data_panel function
      */   
-    public function process_mystyle_data_panel() {
-        //TODO
+    public function test_process_mystyle_data_panel() {
+        global $post;
+        
+        $post_id = 1;
+        
+        //Mock the global $post var and $_POST
+        $post = new stdClass();
+        $post->ID = $post_id;
+        
+        $_POST = array();
+        $_POST['_mystyle_enabled'] = true;
+        $_POST['_mystyle_template_id'] = 1;
+        
+        // Create post object
+        $test_post = array(
+            'ID'            => $post_id,
+            'post_title'    => 'Test post',
+            'post_content'  => 'This is a test post.',
+            'post_status'   => 'publish',
+            'post_author'   => 1,
+            'post_category' => array(8,39)
+        );
+
+        // Insert the post into the database
+        wp_insert_post( $test_post );
+        
+        //Call the function
+        MyStyle_WooCommerce_Admin_Product::process_mystyle_data_panel( $post_id );
+        
+        //Get the post meta
+        $mystyle_enabled = get_post_meta( $post->ID, '_mystyle_enabled', true );
+        $template_id = get_post_meta( $post->ID, '_mystyle_template_id', true );
+       
+        //Assert that the post meta was set
+        $this->assertEquals( 'yes', $mystyle_enabled );
+        $this->assertEquals( 1, $template_id );
     }
     
 }
