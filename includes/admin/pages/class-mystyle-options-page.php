b9567f976781140e7e77f405fbf6781f7c4ded41
sean <sean@onegiantmedia.com>
2015-06-26 20:51:42 -0700

    admin design update

--- includes/admin/pages/class-mystyle-options-page.php
+++ includes/admin/pages/class-mystyle-options-page.php
@@ -7,7 +7,7 @@
  * @since 0.1.0
  */
 class MyStyle_Options_Page {
-    
+
     /**
      * Constructor, constructs the options page and adds it to the Settings
      * menu.
@@ -16,7 +16,7 @@ class MyStyle_Options_Page {
         add_action( 'admin_menu', array( &$this, 'add_page_to_menu' ) );
         add_action( 'admin_init', array( &$this, 'admin_init' ) );
     }
-    
+
     /**
      * Function to initialize the MyStyle options page.
      */
@@ -24,45 +24,46 @@ class MyStyle_Options_Page {
         register_setting( 'mystyle_options', MYSTYLE_OPTIONS_NAME, array( &$this, 'validate' ) );
         add_settings_section(
                 'mystyle_options_access_section',
-                'Access Settings',
+                'MyStyle Account Settings',
                 array( &$this, 'render_access_section_text' ),
                 'mystyle'
         );
         add_settings_field(
-                'api_key', 
-                'API Key', 
+                'api_key',
+                'API Key',
                 array( &$this, 'render_api_key' ),
-                'mystyle', 
+                'mystyle',
                 'mystyle_options_access_section'
         );
         add_settings_field(
-                'secret', 
-                'Secret', 
+                'secret',
+                'Secret',
                 array( &$this, 'render_secret' ),
-                'mystyle', 
+                'mystyle',
                 'mystyle_options_access_section'
         );
     }
-    
+
     /**
      * Function to add the options page to the settings menu.
      */
     public function add_page_to_menu() {
         global $mystyle_hook;
         $mystyle_hook = 'mystyle';
-        
+
         add_menu_page('MyStyle', 'MyStyle', 'manage_options', $mystyle_hook, array( &$this, 'render_page' ), MYSTYLE_ASSETS_URL . '/images/mystyle-icon.png', '56' );
         add_submenu_page( $mystyle_hook, 'Settings', 'Settings', 'manage_options', $mystyle_hook );
         //add_submenu_page( $mystyle_hook, 'Designs', 'Designs', 'manage_product_terms', 'edit-tags.php?taxonomy=product_shipping_class&post_type=product' );
     }
-    
+
     /**
      * Function to render the MyStyle options page.
      */
     public static function render_page() {
     ?>
         <div class="wrap">
-            <div id="icon-options-general" class="icon32"><br /></div><h2>MyStyle Settings</h2>
+            <h2 class="mytyle-admin-title"><div id="icon-options-general" class="icon100"></div> MyStyle Settings</h2>
+            <div class="mystyle-admin-box">
             <form action="options.php" method="post">
                 <?php settings_fields( 'mystyle_options' ); ?>
                 <?php do_settings_sections( 'mystyle' ); ?>
@@ -71,6 +72,7 @@ class MyStyle_Options_Page {
                     <input type="submit" name="Submit" id="submit" class="button button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
                 </p>
             </form>
+            </div>
             <br/>
             <ul>
                 <li>Go to <a href="http://www.mystyleplatform.com/mystyle-personalization-plugin-wordpress-woo-commerce/" target="_blank" title="mystyleplatform.com">mystyleplatform.com</a>.</li>
@@ -86,9 +88,7 @@ class MyStyle_Options_Page {
     public static function render_access_section_text() {
     ?>
         <p>
-            To use MyStyle, you will need a MyStyle API Key and Secret.  To get
-            your MyStyle API Key and Secret, log in or register at 
-            <a href="http://www.mystyleplatform.com/apply-mystyle-license-developer-account-api-key-secret/?ref=wp_plugin_1" target="_blank" title="mystyleplatform.com">mystyleplatform.com</a>.
+            To use MyStyle, you will need to <a href="http://www.mystyleplatform.com/apply-mystyle-license-developer-account-api-key-secret/?ref=wp_plugin_1" target="_blank" title="mystyleplatform.com">register for a developer account</a> to get your own MyStyle API Key and Secret.
         </p>
     <?php
     }
@@ -118,7 +118,7 @@ class MyStyle_Options_Page {
      ?>
         <input id="mystyle_secret" name="mystyle_options[secret]" size="27" type="text" value="<?php echo $secret ?>" />
         <p class="description">
-            You must enter a valid MyStyle Secret here. If you need a MyStyle 
+            You must enter a valid MyStyle Secret here. If you need a MyStyle
             Secret, you can create one
             <a href="http://www.mystyleplatform.com/apply-mystyle-license-developer-account-api-key-secret/?ref=wp_plugin_3" target="_blank" title="MyStyle Signup">here</a>.
         </p>
@@ -126,10 +126,10 @@ class MyStyle_Options_Page {
     }
 
     /**
-     * Function to validate the submitted MyStyle options field values. 
-     * 
+     * Function to validate the submitted MyStyle options field values.
+     *
      * This function overrites the old values instead of completely replacing them so
-     * that we don't overwrite values that weren't submitted (such as the 
+     * that we don't overwrite values that weren't submitted (such as the
      * version).
      * @param array $input The submitted values
      * @return array Returns the new options to be stored in the database.
@@ -137,13 +137,13 @@ class MyStyle_Options_Page {
     public static function validate( $input ) {
         $old_options = get_option( MYSTYLE_OPTIONS_NAME );
         $new_options = $old_options;  //start with the old options.
-        
+
         $has_errors = false;
         $msg_type = null;
         $msg_message = null;
-        
+
         //------------ process the new values ------------
-        
+
         //API Key
         $new_options['api_key'] = trim( $input['api_key'] );
         if(!preg_match( '/^[a-z0-9]*$/i', $new_options['api_key'] ) ) {
@@ -152,7 +152,7 @@ class MyStyle_Options_Page {
             $msg_message = 'Please enter a valid API Key.';
             $new_options['api_key'] = '';
         }
-        
+
         //Secret
         $new_options['secret'] = trim( $input['secret'] );
         if( ! preg_match('/^[a-z0-9]*$/i', $new_options['secret'] ) ) {
@@ -161,19 +161,19 @@ class MyStyle_Options_Page {
             $msg_message = 'Please enter a valid Secret.';
             $new_options['secret'] = '';
         }
-        
+
         if(!$has_errors) {
             $msg_type = 'updated';
             $msg_message = 'Settings saved.';
         }
-        
+
         add_settings_error(
             'MyStyleOptionsSaveMessage',
             esc_attr('settings_updated'),
             $msg_message,
             $msg_type
         );
-        
+
         return $new_options;
     }
 }
