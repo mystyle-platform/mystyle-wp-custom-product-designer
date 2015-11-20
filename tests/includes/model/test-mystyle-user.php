ea1193ba413d7239db0ea44ffda59f192421f600
Collin Krawll <collin.krawll@thirdset.com>
2015-11-20 13:31:44 -0800

    Added MyStyle_User class and unit tests for it and the API calls to get it.

---
+++ tests/includes/model/test-mystyle-user.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The MyStyleUserTest class includes tests for testing the MyStyle_User
+ * class.
+ *
+ * @package MyStyle
+ * @since 1.2.0
+ */
+class MyStyleUserTest extends WP_UnitTestCase {
+
+    /**
+     * Test the create function
+     */    
+    function test_create() {
+        $user_id = 1;
+        $email = 'someone@example.com';
+        
+        $user = new MyStyle_User( $user_id, $email );
+        
+        //Assert that the user is constructed
+        $this->assertEquals( 'MyStyle_User', get_class( $user ) );
+    }
+    
+}
