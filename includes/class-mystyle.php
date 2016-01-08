0cc9b0d7814cd3e4716691924823933010d08b5b
Collin Krawll <collin.krawll@thirdset.com>
2016-01-08 10:44:25 -0800

    Fixed bug with the thumbnail image in latest WP (caused by srcset attribute)

--- includes/class-mystyle.php
+++ includes/class-mystyle.php
@@ -85,9 +85,12 @@ class MyStyle {
             
             $design = MyStyle_DesignManager::get( $design_id );
 
+            //overwrite the src attribute
             $new_src = 'src="' . $design->get_thumb_url() . '"';
-	
-            $new_image_tag = preg_replace( '/src\=".*?"/', $new_src, $get_image );
+            $new_image_tag = preg_replace( '/src\=".*?"/', $new_src, $new_image_tag );
+            
+            //remove the srcset attribute
+            $new_image_tag = preg_replace( '/srcset\=".*?"/', '', $new_image_tag );
         }
 	
         return $new_image_tag;
