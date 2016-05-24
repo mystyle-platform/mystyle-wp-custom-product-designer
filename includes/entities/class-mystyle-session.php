a2e9ac5bc10566a71ff1ad3e8ca53df3cb2a18b5
Collin Krawll <collin.krawll@thirdset.com>
2016-05-24 13:33:26 -0700

    Fixed a bug with the generate_session_id() function.

--- includes/entities/class-mystyle-session.php
+++ includes/entities/class-mystyle-session.php
@@ -205,71 +205,42 @@ class MyStyle_Session implements MyStyle_Entity {
     
     /**
      * Generates a session id.
-     * Source: https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/drupal_random_bytes/7.x
+     * Sources: 
+     *  * https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/drupal_random_bytes/7.x
+     *  * http://stackoverflow.com/questions/48124/generating-pseudorandom-alpha-numeric-strings
      * @return string Returns the generated session id.
      */
     public static function generate_session_id()
     {
+        $bytes = '';
+        $sid = '';
+        
         //the number of bytes that we want
         $count = 43;
-        
-        static $random_state, $bytes, $has_openssl;
 
-        $missing_bytes = $count - strlen( $bytes );
+        // use openssl_random_pseudo_bytes if available.
+        // Note: PHP versions prior 5.3.4 experienced openssl_random_pseudo_bytes()
+        // locking on Windows and rendered it unusable.
+        if ( version_compare( PHP_VERSION, '5.3.4', '>=' ) && function_exists( 'openssl_random_pseudo_bytes' ) ) {
+            $bytes = openssl_random_pseudo_bytes( $count );
+            $sid   = bin2hex($bytes);
+        } else {
+            // range is numbers (48) through capital and lower case letters (122)
+            $range_start = 48;
+            $range_end   = 122;
 
-        if ( $missing_bytes > 0 ) {
-            // PHP versions prior 5.3.4 experienced openssl_random_pseudo_bytes()
-            // locking on Windows and rendered it unusable.
-            if ( !isset( $has_openssl ) ) {
-                $has_openssl = version_compare( PHP_VERSION, '5.3.4', '>=' ) && function_exists( 'openssl_random_pseudo_bytes' );
-            }
-
-            // openssl_random_pseudo_bytes() will find entropy in a system-dependent
-            // way.
-            if ( $has_openssl ) {
-                $bytes .= openssl_random_pseudo_bytes( $missing_bytes );
-            }
-
-            // Else, read directly from /dev/urandom, which is available on many *nix
-            // systems and is considered cryptographically secure.
-            elseif ( $fh = @fopen( '/dev/urandom', 'rb' ) ) {
-                // PHP only performs buffered reads, so in reality it will always read
-                // at least 4096 bytes. Thus, it costs nothing extra to read and store
-                // that much so as to speed any additional invocations.
-                $bytes .= fread( $fh, max( 4096, $missing_bytes ) );
-                fclose( $fh );
-            }
-
-            // If we couldn't get enough entropy, this simple hash-based PRNG will
-            // generate a good set of pseudo-random bytes on any system.
-            // Note that it may be important that our $random_state is passed
-            // through hash() prior to being rolled into $output, that the two hash()
-            // invocations are different, and that the extra input into the first one -
-            // the microtime() - is prepended rather than appended. This is to avoid
-            // directly leaking $random_state via the $output stream, which could
-            // allow for trivial prediction of further "random" numbers.
-            if ( strlen( $bytes ) < $count ) {
-                // Initialize on the first call. The contents of $_SERVER includes a mix of
-                // user-specific and system information that varies a little with each page.
-                if ( ! isset( $random_state ) ) {
-                    $random_state = print_r( $_SERVER, TRUE );
-                    if ( function_exists( 'getmypid' ) ) {
-                        // Further initialize with the somewhat random PHP process ID.
-                        $random_state .= getmypid();
-                    }
-                    $bytes = '';
-                }
-
-                do {
-                    $random_state = hash( 'sha256', microtime() . mt_rand() . $random_state );
-                    $bytes .= hash( 'sha256', mt_rand() . $random_state, TRUE );
-                }
-                while ( strlen( $bytes ) < $count );
+            for ( $i = 0; $i < $count; $i++ ) {
+              // generate a number within the range
+              $ascii_no = round( mt_rand( $range_start , $range_end ) ); 
+              // get the char by number and add it to the session id.
+              $sid .= chr( $ascii_no );
             }
         }
-        $output = substr( $bytes, 0, $count );
-        $bytes = substr( $bytes, $count );
-        return $output;
+        
+        //ensure that the new sid is not longer than $count
+        $sid = substr( $sid, 0, $count );
+
+        return $sid;
     }
 
 }
