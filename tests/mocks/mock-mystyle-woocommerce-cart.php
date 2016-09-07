04329d598ad24632f4aa821f31dd8b81312d47cd
Collin Krawll <collin.krawll@thirdset.com>
2016-09-07 10:24:19 -0700

    Added the get_cart() method to the MyStyle_MockWooCommerceCart class.

--- tests/mocks/mock-mystyle-woocommerce-cart.php
+++ tests/mocks/mock-mystyle-woocommerce-cart.php
@@ -12,12 +12,32 @@ class MyStyle_MockWooCommerceCart {
         //
     }
     
+    /**
+     * Mock the add_to_cart method.
+     * @param type $product_id
+     * @param type $some_int
+     * @param type $some_string
+     * @param type $some_array
+     * @param type $cart_item_data
+     */
     public function add_to_cart( $product_id, $some_int, $some_string, $some_array, $cart_item_data ) {
         // do nothing
     }
     
+    /**
+     * Mock the get_cart_url method.
+     * @return string
+     */
     public function get_cart_url() {
         return 'mock_cart_url';
+    }
+    
+    /**
+     * Mock the get_cart method.
+     * @return type
+     */
+    public function get_cart() {
+        return array();
     }
 
 }
