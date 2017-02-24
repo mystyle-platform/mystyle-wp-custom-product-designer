85221f192eb57b3d87aa6095c706e074053c1739
Collin Krawll <collin.krawll@thirdset.com>
2017-02-23 17:24:18 -0800

    When retrieving sessions, we now catch exceptions, fail silently, log the exception, and recreate the session.

--- includes/class-mystyle-sessionhandler.php
+++ includes/class-mystyle-sessionhandler.php
@@ -22,21 +22,32 @@ class MyStyle_SessionHandler {
      */
     public static function get() {
         if(session_id() == '') {
+            //require the MyStyle_Session class again just in case.
+            require_once( MYSTYLE_INCLUDES . 'entities/class-mystyle-session.php' );
             session_start();
         }
         
         $session = null;
         
-        //first look in the session variables
-        if( isset( $_SESSION[MyStyle_Session::$SESSION_KEY] ) ) {    
-            $session = $_SESSION[MyStyle_Session::$SESSION_KEY];
-        } else {
-            //next look in their cookies
-            if( isset( $_COOKIE[MyStyle_Session::$COOKIE_NAME] ) ) {
-                $session_id = $_COOKIE[MyStyle_Session::$COOKIE_NAME];
-                $session = MyStyle_SessionManager::get( $session_id );
-                $_SESSION[MyStyle_Session::$SESSION_KEY] = $session;
+        try {
+            //first look in the session variables
+            if( isset( $_SESSION[MyStyle_Session::$SESSION_KEY] ) ) {    
+                $session = $_SESSION[MyStyle_Session::$SESSION_KEY];
+            } else {
+                //next look in their cookies
+                if( isset( $_COOKIE[MyStyle_Session::$COOKIE_NAME] ) ) {
+                    $session_id = $_COOKIE[MyStyle_Session::$COOKIE_NAME];
+                    $session = MyStyle_SessionManager::get( $session_id );
+                    $_SESSION[MyStyle_Session::$SESSION_KEY] = $session;
+                }
             }
+            
+        } catch (\Exception $ex) {
+            //if an unexpected exception occurs when trying to retrieve the
+            //session, fail silently, write to the log, and null out the 
+            //(possibly corrupted) session. In this scenario, a new session
+            //will be created below as if there never was one.
+            error_log('Exception caught while trying to retrieve the user\'s session ' . $ex);
         }
         
         //Version 1.3.0 - 1.3.4 had an issue where it was creating binary
