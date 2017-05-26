0111183bec5e97e3c3bdeed4230b828d01b4f7bc
Collin Krawll <collin.krawll@thirdset.com>
2017-05-26 13:38:43 -0700

    Added a new remove_user_register_actions function to the MyStyle_Test_Util class.

--- tests/class-mystyle-test-util.php
+++ tests/class-mystyle-test-util.php
@@ -11,6 +11,16 @@
 abstract class MyStyle_Test_Util {
     
     /**
+     * Remove all actions that we've hooked to the 'user_register' event.
+     * 
+     * This keeps code that isn't part of the SUT (System Under Test) from
+     * executing.
+     */
+    public static function remove_user_register_actions() {
+        remove_action( 'user_register', array( MyStyle_User_Interface::get_instance() , 'on_user_register' ) );
+    }
+    
+    /**
      * Creates a test user while removing our hooks.
      * @param type $username The username that you want for the test user.
      * @param type $password The password that you want for the test user.
@@ -18,7 +28,7 @@ abstract class MyStyle_Test_Util {
      * @return integer Returns the user_id of the created test_user.
      */
     public static function create_user( $username, $password, $email ) {
-        remove_action( 'user_register', array( MyStyle_User_Interface::get_instance() , 'on_user_register' ) );
+        self::remove_user_register_actions();
         
         //Mock the user (note this will call the function since it is hooked into the register function)
         $user_id = wp_create_user( 'testuser', 'testpassword', $email );
