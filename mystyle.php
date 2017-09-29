0bd15864719e1d58dc97fdc57a5b0b3ac711dde9
Collin Krawll <collin.krawll@thirdset.com>
2017-09-28 17:27:34 -0700

    Added 'WC requires at least: 2.2.0' and 'WC tested up to: 3.1.2' to the mystyle.php file.

--- mystyle.php
+++ mystyle.php
@@ -5,6 +5,8 @@ Plugin Name: MyStyle
 Plugin URI: http://www.mystyleplatform.com
 Description: The MyStyle Custom Product Designer is a simple plugin that allows your customers to customize products in WooCommerce.
 Version: 2.1.2
+WC requires at least: 2.2.0
+WC tested up to: 3.1.2
 Author: mystyleplatform
 Author URI: www.mystyleplatform.com
 License: GPL v3
