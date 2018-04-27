1e1b3a397ef3aff27676c399f296eb8a21a23703
Collin Krawll <collin.krawll@thirdset.com>
2018-04-27 14:34:28 -0700

    Added a new MyStyleDesignCompleteTest class to test the MyStyle_Design_Complete class.

---
+++ tests/includes/frontend/test-mystyle-design-complete.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once( MYSTYLE_INCLUDES . 'frontend/class-mystyle-design-complete.php' );
+
+/**
+ * The MyStyleDesignComplete class includes tests for testing the
+ * MyStyle_Design_Complete class.
+ *
+ * @package MyStyle
+ * @since 1.5.0
+ */
+class MyStyleDesignCompleteTest extends WP_UnitTestCase {
+    
+    /**
+     * Test the constructor.
+     * @global $wp_filter
+     */    
+    public function test_constructor() {
+        global $wp_filter;
+        
+        //Call the constructor.
+        $mystyle_design_complete = new MyStyle_Design_Complete();
+        
+        //Assert that the filter_cart_button_text function is registered.
+        $function_names = get_function_names( $wp_filter['query_vars'] );
+        $this->assertContains( 'add_query_vars_filter', $function_names );
+        
+        //Assert that the init function is registered.
+        $function_names = get_function_names( $wp_filter['template_redirect'] );
+        $this->assertContains( 'init', $function_names );
+    }
+    
+    /**
+     * Test the init function.
+     * @global $wp_scripts
+     */    
+    public function test_init() {
+        global $wp_scripts;
+        
+        //Instantiate the SUT (System Under Test) class.
+        $mystyle_design_complete = new MyStyle_Design_Complete();
+        
+        //Mock the query var
+        set_query_var( 'design_complete', 1 );
+        
+        //Call the method
+        $mystyle_design_complete->init();
+        
+        //Assert that the design-complete.js script is registered
+        $this->assertContains( 
+                'mystyle-design-complete', 
+                serialize( $wp_scripts ) 
+            );
+    }
+    
+    /**
+     * Test the add_query_vars_filter function.
+     */
+    public function test_add_query_vars_filter( ) {
+        
+        $vars[] = array();
+        
+        //call the function
+        $ret_vars = MyStyle_Design_Complete::get_instance()->add_query_vars_filter( $vars );
+        
+        $this->assertTrue( in_array( 'design_complete', $ret_vars ) );
+    }
+    
+}
