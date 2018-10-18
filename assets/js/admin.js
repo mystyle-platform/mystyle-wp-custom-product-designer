58843d83d8b837eb6b8cc56169a25fbb1a156821
Collin Krawll <collin.krawll@thirdset.com>
2018-10-18 14:16:54 -0700

    Added a PHP Code Sniffer configuration file (phpcs.xml) for checking the code against the WP coding standards. Fixed the formatting of the js files.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,17 +4,16 @@
  * @returns {Boolean} Returns false;
  */
 function mystyleTogglePanelVis(id) {
-    var panelObj = document.getElementById('mystyle-panel-' + id);
-    var toggleObj = document.getElementById('mystyle-toggle-handle-' + id);
+	var panelObj = document.getElementById('mystyle-panel-' + id);
+	var toggleObj = document.getElementById('mystyle-toggle-handle-' + id);
 
-    if (panelObj.style.display == 'none') { //open the data
-        panelObj.style.display = 'block';
-        toggleObj.className += ' mystyle-closed';
-    } else { //close the data
-        panelObj.style.display = 'none';
-        toggleObj.className = toggleObj.className.replace(/(?:^|\s)mystyle\-closed(?!\S)/g, '');
-    }
+	if ( panelObj.style.display == 'none' ) { //open the data
+		panelObj.style.display = 'block';
+		toggleObj.className += ' mystyle-closed';
+	} else { //close the data
+		panelObj.style.display = 'none';
+		toggleObj.className = toggleObj.className.replace( /(?:^|\s)mystyle\-closed(?!\S)/g, '' );
+	}
 
-    return false;
+	return false;
 }
-
--- assets/js/design-complete.js
+++ assets/js/design-complete.js
@@ -4,120 +4,122 @@
  * @since 3.4.0
  */
 
-MyStyleDesignComplete = function () {
+MyStyleDesignComplete = function() {
 
-    /**
-     * Private object for attaching private properties and methods
-     * @var {object}
-     */
-    var self = {};
+	/**
+	 * Private object for attaching private properties and methods.
+	 * 
+	 * @var {object}
+	 */
+	var self = {};
 
-    self.designId = null;
+	self.designId = null;
 
-    /**
-     * EXPOSED (as 'init')
-     * 
-     * Initializes the design complete page.
-     * 
-     * Does the following:
-     *  * Finds any forms and adds the design_id as a hidden field.
-     *  
-     */
-    self._init = function () {
+	/**
+	 * EXPOSED (as 'init')
+	 * 
+	 * Initializes the design complete page.
+	 * 
+	 * Does the following:
+	 *  * Finds any forms and adds the design_id as a hidden field.
+	 *  
+	 */
+	self._init = function() {
 
-        // Validate the query string vars before continuing
-        // This will throw an exception if the vars are missing or invalid.
-        self._validateQueryVars();
+		// Validate the query string vars before continuing
+		// This will throw an exception if the vars are missing or invalid.
+		self._validateQueryVars();
 
-        // Pull the designId from the current url
-        self.designId = self._getQueryVariable('design_id');
+		// Pull the designId from the current url.
+		self.designId = self._getQueryVariable( 'design_id' );
 
-        // Set the value of any design_id fields.
-        self._setDesignIdFields();
-    };
+		// Set the value of any design_id fields.
+		self._setDesignIdFields();
+	};
 
-    /**
-     * Private helper method that gets a variable from the current query string
-     * (via window.location).
-     * Source: https://css-tricks.com/snippets/javascript/get-url-variables/
-     * @param {string} variable The name of the variable that you want to get.
-     * @returns {mixed} Returns the variable value or false if the variable isn't
-     * found.
-     */
-    self._getQueryVariable = function (variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split('=');
-            if (pair[0] === variable) {
-                return pair[1];
-            }
-        }
+	/**
+	 * Private helper method that gets a variable from the current query string
+	 * (via window.location).
+	 * Source: https://css-tricks.com/snippets/javascript/get-url-variables/
+	 * 
+	 * @param {string} variable The name of the variable that you want to get.
+	 * @returns {mixed} Returns the variable value or false if the variable isn't
+	 * found.
+	 */
+	self._getQueryVariable = function( variable ) {
+		var query = window.location.search.substring(1);
+		var vars = query.split( '&' );
+		for ( var i = 0; i < vars.length; i++ ) {
+			var pair = vars[i].split( '=' );
+			if ( pair[0] === variable ) {
+				return pair[1];
+			}
+		}
 
-        return(false);
-    };
+		return( false );
+	};
 
-    /**
-     * Private helper method that validates the query args.
-     * @returns {boolean} Returns true if the query args are valid, otherwise,
-     * returns false.
-     */
-    self._validateQueryVars = function () {
+	/**
+	 * Private helper method that validates the query args.
+	 * 
+	 * @returns {boolean} Returns true if the query args are valid, otherwise,
+	 * returns false.
+	 */
+	self._validateQueryVars = function() {
 
-        // Validate the design_complete query var
-        if (self._getQueryVariable('design_complete') !== '1') {
-            throw 'MyStyleDesignComplete: Query vars are not valid (must include "design_complete=1").';
-        }
+		// Validate the design_complete query var.
+		if ( self._getQueryVariable( 'design_complete' ) !== '1' ) {
+			throw 'MyStyleDesignComplete: Query vars are not valid (must include "design_complete=1").';
+		}
 
-        // Validate the design_id query var
-        var designId = self._getQueryVariable('design_id');
-        if (!designId) {
-            throw 'MyStyleDesignComplete: Query vars are not valid (must include design_id).';
-        }
-        if (isNaN(designId)) {
-            throw 'MyStyleDesignComplete: Query vars are not valid (design_id must be a number).';
-        }
+		// Validate the design_id query var.
+		var designId = self._getQueryVariable( 'design_id' );
+		if ( ! designId ) {
+			throw 'MyStyleDesignComplete: Query vars are not valid (must include design_id).';
+		}
+		if ( isNaN( designId ) ) {
+			throw 'MyStyleDesignComplete: Query vars are not valid (design_id must be a number).';
+		}
 
-        return true;
-    };
+		return true;
+	};
 
-    /**
-     * Private helper method that sets the value of any design_id fields.
-     */
-    self._setDesignIdFields = function () {
+	/**
+	 * Private helper method that sets the value of any design_id fields.
+	 */
+	self._setDesignIdFields = function() {
 
-        jQuery('input').each(function (index, value) {
+		jQuery( 'input' ).each( function ( index, value ) {
 
-            // Any field with 'design_id' in the id.
-            var id = jQuery(this).attr('id');
-            if ((typeof id !== 'undefined') && (id.indexOf('design_id') !== -1)) {
-                jQuery(this).val(self.designId);
-                return true;
-            }
+			// Any field with 'design_id' in the id.
+			var id = jQuery( this ).attr( 'id' );
+			if ( ( typeof id !== 'undefined' ) && ( id.indexOf( 'design_id' ) !== -1 ) ) {
+				jQuery( this ).val( self.designId );
+				return true;
+			}
 
-            // Any field with 'design_id' in the name attribute.
-            var name = jQuery(this).attr('name');
-            if ((typeof name !== 'undefined') && (name.indexOf('design_id') !== -1)) {
-                jQuery(this).val(self.designId);
-            }
-        });
-    };
+			// Any field with 'design_id' in the name attribute.
+			var name = jQuery( this ).attr( 'name' );
+			if ( ( typeof name !== 'undefined' ) && ( name.indexOf( 'design_id' ) !== -1 ) ) {
+				jQuery( this ).val( self.designId );
+			}
+		});
+	};
 
-    /**
-     * Declare the publicly exposed return object.
-     */
-    self.public = {
-        init: function () {
-            return self._init();
-        }
-    };
+	/**
+	 * Declare the publicly exposed return object.
+	 */
+	self.public = {
+		init: function () {
+			return self._init();
+		}
+	};
 
-    return self.public;
+	return self.public;
 
 }();
 // End MyStyleDesignComplete class.
 
-jQuery(window).ready(function () {
-    MyStyleDesignComplete.init();
+jQuery( window ).ready( function () {
+	MyStyleDesignComplete.init();
 });
-
