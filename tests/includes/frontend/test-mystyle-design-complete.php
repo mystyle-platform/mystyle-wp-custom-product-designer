9998f2c582553aa635b8c259d23814806f2eef75
Collin Krawll <collin.krawll@thirdset.com>
2019-01-16 17:56:31 -0800

    Added a unit test to ensure that the product specific design complete url is picked up.

--- tests/includes/frontend/test-mystyle-design-complete.php
+++ tests/includes/frontend/test-mystyle-design-complete.php
@@ -18,6 +18,31 @@ require_once MYSTYLE_INCLUDES . 'frontend/class-mystyle-design-complete.php';
 class MyStyleDesignCompleteTest extends WP_UnitTestCase {
 
 	/**
+	 * Overwrite the setUp function so that our custom tables will be persisted
+	 * to the test database.
+	 */
+	public function setUp() {
+		// Perform the actual task according to parent class.
+		parent::setUp();
+
+		// Create the tables.
+		MyStyle_Install::create_tables();
+	}
+
+	/**
+	 * Overwrite the tearDown function to remove our custom tables.
+	 */
+	public function tearDown() {
+		global $wpdb;
+		// Perform the actual task according to parent class.
+		parent::tearDown();
+
+		// Drop the tables that we created.
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . MyStyle_Design::get_table_name() );
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . MyStyle_Session::get_table_name() );
+	}
+
+	/**
 	 * Test the constructor.
 	 *
 	 * @global $wp_filter
@@ -101,7 +126,7 @@ class MyStyleDesignCompleteTest extends WP_UnitTestCase {
 	 * that already includes a query string.
 	 */
 	public function test_get_redirect_url_with_url_that_includes_a_query_string() {
-		// Install the alternate_design_complete_redirect_url.
+		// Install a global redirect url.
 		$options = array();
 		update_option( MYSTYLE_OPTIONS_NAME, $options );
 		$options['alternate_design_complete_redirect_url'] = 'http://www.example.com?foo=bar';
@@ -110,10 +135,45 @@ class MyStyleDesignCompleteTest extends WP_UnitTestCase {
 		// Create a design.
 		$design = MyStyle_MockDesign::get_mock_design( 1 );
 
+		// Create a product and set the product specific redirect url.
+		$product_id      = create_wc_test_product();
+		$product         = new \WC_Product_Simple( $product_id );
+		add_post_meta(
+			$product_id,
+			'_mystyle_customizer_redirect',
+			'http://www.example.net?foo=bar'
+		);
+		$design->set_product_id( $product_id );
+
 		$url = MyStyle_Design_Complete::get_redirect_url( $design );
 
 		$this->assertEquals(
-			'http://www.example.com?foo=bar&design_id=1&design_complete=1',
+			'http://www.example.net?foo=bar&design_id=1&design_complete=1',
+			$url
+		);
+	}
+
+	/**
+	 * Assert that get_redirect_url() returns the expected URL when a product
+	 * specific URL is set.
+	 */
+	public function test_get_redirect_url_with_product_specific_url() {
+		// Install the alternate_design_complete_redirect_url.
+		$options = array();
+		update_option( MYSTYLE_OPTIONS_NAME, $options );
+		$options['alternate_design_complete_redirect_url'] = 'http://www.example.com';
+		update_option( MYSTYLE_OPTIONS_NAME, $options );
+
+		// Create a design.
+		$design = MyStyle_MockDesign::get_mock_design( 1 );
+
+		// Set a redirect url on the design's product.
+
+
+		$url = MyStyle_Design_Complete::get_redirect_url( $design );
+
+		$this->assertEquals(
+			'http://www.example.com?design_id=1&design_complete=1',
 			$url
 		);
 	}
