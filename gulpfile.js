7b34f5bbd757651124566d5f44e7d1aa480a94f3
Collin Krawll <collin.krawll@thirdset.com>
2019-02-06 16:35:12 -0800

    Added eslint and eslint-config-wordpress.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var stylelint = require('gulp-stylelint');
 var fs = require('fs');
 
@@ -10,13 +11,20 @@ gulp.task('default', function(done) {
 gulp.task('lint', function(done) {
     console.log('Linting...');
     
-    // Lint our css files.
-    gulp.src(['assets/**/*.css'])
+    // Lint our js files.
+	gulp.src(['assets/js/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError())
+		.on('error', function(e){console.log(e);});
+	
+	// Lint our css files.
+    gulp.src(['assets/css/*.css'])
 		// Lint the file.
 		.pipe(stylelint({
-		  reporters: [
-			{formatter: 'string', console: true}
-		  ]
+			reporters: [
+				{formatter: 'string', console: true}
+			]
 		}))
 		.on('error', function(e){console.log(e);});
 
