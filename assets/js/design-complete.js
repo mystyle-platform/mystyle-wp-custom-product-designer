ef03b89bef700d26a21ba9ed965bffa7724cafd2
Collin Krawll <collin.krawll@thirdset.com>
2019-02-06 18:06:28 -0800

    Added JS testign with Karma, Mocha and Chai.

--- assets/js/design-complete.js
+++ assets/js/design-complete.js
@@ -25,11 +25,11 @@ MyStyleDesignComplete = function() {
 	 *  * Finds any forms and adds the design_id as a hidden field.
 	 */
 	self._init = function() {
-
+console.log(1);
 		// Validate the query string vars before continuing
 		// This will throw an exception if the vars are missing or invalid.
 		self._validateQueryVars();
-
+console.log(2);
 		// Pull the designId from the current url.
 		self.designId = self._getQueryVariable( 'design_id' );
 
@@ -123,6 +123,8 @@ MyStyleDesignComplete = function() {
 // End MyStyleDesignComplete class.
 jQuery( window ).ready(
 	function () {
+		if ( ! window.hasOwnProperty('__karma__') ) {
 			MyStyleDesignComplete.init();
+		}
 	}
 );
---
+++ karma.conf.js
@@ -0,0 +1,82 @@
+// Karma configuration.
+
+module.exports = function(config) {
+    config.set({
+
+        // Base path that will be used to resolve all patterns (eg. files, exclude).
+        basePath: '',
+
+
+        // Frameworks to use.
+        // Available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['mocha', 'chai'],
+
+
+        // List of files / patterns to load in the browser.
+        files: [
+            
+            // Libraries.
+            '../../../wp-includes/js/jquery/jquery.js',
+            
+            // Our scripts.
+			'assets/js/admin.js',
+            'assets/js/customize.js',
+			'assets/js/design-complete.js',
+            
+            // The tests.
+            'javascript-tests/**/*.js'
+        ],
+
+
+        // List of files to exclude.
+        exclude: [
+        ],
+
+
+        // Preprocess matching files before serving them to the browser.
+        // Available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
+
+
+        // Test results reporter to use.
+        // Possible values: 'dots', 'progress'
+        // Available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // Web server port.
+        port: 9876,
+
+
+        // Enable / disable colors in the output (reporters and logs).
+        colors: true,
+
+
+        // Level of logging.
+        // Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // Enable / disable watching file and executing tests whenever any file changes.
+        autoWatch: true,
+
+
+        // Start these browsers.
+        // Available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode.
+        // If true, Karma captures browsers, runs the tests and exits.
+        singleRun: false,
+        
+        client: {
+            captureConsole: true,
+            mocha: {
+                bail: true
+            }
+        }
+        
+    });
+};
