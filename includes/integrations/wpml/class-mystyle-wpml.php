809b0a9eaaf8b2a0b454cc3975caf080ec8ad83f
Collin Krawll <collin.krawll@thirdset.com>
2019-10-30 16:14:34 -0700

    Added functions to the MyStyle_Wpml class to test if WPML is installed and to test if a passed page is a translation of some other passed page.

--- includes/integrations/wpml/class-mystyle-wpml.php
+++ includes/integrations/wpml/class-mystyle-wpml.php
@@ -13,11 +13,100 @@
 class MyStyle_Wpml {
 
 	/**
+	 * The name of the db table where this WPML translations are stored.
+	 *
+	 * Note: this is without the db prefix.
+	 *
+	 * @var string
+	 */
+	const TRANSLATIONS_TABLE_NAME = 'icl_translations';
+
+	/**
 	 * Singleton class instance.
 	 *
 	 * @var MyStyle_Wpml
 	 */
 	private static $instance;
+
+	/**
+	 * Returns the name of the WPML translations table.
+	 *
+	 * @global type $wpdb
+	 * @return string Returns the name of the WPML translations table.
+	 */
+	public function get_translations_table_name() {
+		global $wpdb;
+
+		return $wpdb->prefix . self::TRANSLATIONS_TABLE_NAME;
+	}
+
+	/**
+	 * Function that determines if the the WPML plugin is installed.
+	 *
+	 * Note: we test against database tables so that this function will work
+	 * regardless of plugin load order.
+	 *
+	 * @returns boolean Returns true if the WPML plugin is installed. Otherwise,
+	 * returns false.
+	 * @global type $wpdb
+	 */
+	public function is_installed() {
+		global $wpdb;
+
+		$table_name = $this->get_translations_table_name();
+
+		$stmt = $wpdb->prepare( 'SHOW TABLES LIKE \'%s\'', $table_name );
+
+		$found_table_name = $wpdb->get_var( $stmt );
+
+		if ( $found_table_name === $table_name ) {
+			$is_installed = true;
+		} else {
+			$is_installed = false;
+		}
+
+		return $is_installed;
+	}
+
+	/**
+	 * Function that determines if the page with an id of $translation_id is a
+	 * translation of the page with an id of $parent_id.
+	 *
+	 * @param int $parent_id The post id of the parent page.
+	 * @param int $translation_id The post id of the page that might be a
+	 * translation.
+	 * @returns boolean Returns true if the page is a translation of the
+	 * parent page. Returns false if the page is not a translation of the parent
+	 * page. Also returns false if WPML isn't found.
+	 * @global type $wpdb
+	 */
+	public function is_translation_of_page( $parent_id, $translation_id ) {
+		global $wpdb;
+
+		// If WPML isn't installed, just return false.
+		if ( ! $this->is_installed() ) {
+			return false;
+		}
+
+		$stmt = $wpdb->prepare(
+			"SELECT 1
+			 FROM {$this->get_translations_table_name()}
+			 WHERE element_type = 'post_page'
+			 AND element_id = %d
+			 AND trid = (SELECT trid
+				FROM {$this->get_translations_table_name()}
+				WHERE element_type = 'post_page'
+				AND element_id = %d
+				)
+			",
+			$translation_id, $parent_id
+		);
+		$ret = $wpdb->query($stmt);
+
+		$is_translation_of_page = boolval($ret);
+
+		return boolval($ret);
+	}
 
 	/**
 	 * Resets the singleton instance. This is used during testing if we want to
--- tests/includes/integrations/wpml/test-mystyle-wpml.php
+++ tests/includes/integrations/wpml/test-mystyle-wpml.php
@@ -13,6 +13,39 @@
 class MyStyleWpmlTest extends WP_UnitTestCase {
 
 	/**
+	 * Overwrite the setUp function so that our custom tables will be persisted
+	 * to the test database.
+	 *
+	 * Note: we need our tables because some of the functions here invoke hooks
+	 * that need the tables.
+	 */
+	public function setUp() {
+		// Perform the actual task according to parent class.
+		parent::setUp();
+		// Remove filters that will create temporary tables. So that permanent
+		// tables will be created.
+		remove_filter( 'query', array( $this, '_create_temporary_tables' ) );
+		remove_filter( 'query', array( $this, '_drop_temporary_tables' ) );
+
+		// Create the tables.
+		$this->create_tables();
+	}
+
+	/**
+	 * Overwrite the tearDown function to remove our custom tables.
+	 *
+	 * @global $wpdb
+	 */
+	public function tearDown() {
+		global $wpdb;
+		// Perform the actual task according to parent class.
+		parent::tearDown();
+
+		// Drop the tables that we created.
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . MyStyle_Wpml::get_instance()->get_translations_table_name() );
+	}
+
+	/**
 	 * Test the constructor.
 	 */
 	public function test_constructor() {
@@ -24,5 +57,169 @@ class MyStyleWpmlTest extends WP_UnitTestCase {
 			'MyStyle_Wpml',
 			get_class( $mystyle_wpml )
 		);
+	}
+
+	/**
+	 * Test the get_translations_table_name function.
+	 */
+	public function test_get_translations_table_name() {
+		$expected_table_name = 'wptests_icl_translations';
+
+		// Call the method.
+		$table_name = MyStyle_Wpml::get_instance()->get_translations_table_name();
+
+		// Assert that the expected table name is returned.
+		$this->assertEquals( $expected_table_name, $table_name );
+	}
+
+	/**
+	 * Test the is_installed function when the WPML plugin is installed.
+	 */
+	public function test_is_installed_returns_true_when_installed() {
+		// Call the method.
+		$is_installed = MyStyle_Wpml::get_instance()->is_installed();
+
+		// Assert that the method returned true as expected.
+		$this->assertTrue( $is_installed );
+	}
+
+	/**
+	 * Test the is_installed function when the WPML plugin is installed.
+	 *
+	 * @global $wpdb
+	 */
+	public function test_is_installed_returns_false_when_not_installed() {
+		global $wpdb;
+
+		// Drop the WPML table (added in the setUp method above).
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . MyStyle_Wpml::get_instance()->get_translations_table_name() );
+
+		// Call the method.
+		$is_installed = MyStyle_Wpml::get_instance()->is_installed();
+
+		// Assert that the method returned false as expected.
+		$this->assertFalse( $is_installed );
+	}
+
+	/**
+	 * Test that the is_translation_of_page function returns false when the test
+	 * page is not a translation of the parent page.
+	 */
+	public function test_is_translation_of_page_returns_false_when_not_a_translation() {
+		// Set up the test data.
+		$parent_id = 1;
+		$translation_id = 2;
+
+		// Call the method.
+		$ret = MyStyle_Wpml::get_instance()->is_translation_of_page( $parent_id, $translation_id );
+
+		// Assert that the method returned false as expected.
+		$this->assertFalse( $ret );
+	}
+
+	/**
+	 * Test that the is_translation_of_page function returns true when the test
+	 * page is a translation of the parent page.
+	 *
+	 * @global $wpdb
+	 */
+	public function test_is_translation_of_page_returns_true_when_is_a_translation() {
+		global $wpdb;
+
+		// Set up the test data.
+		$parent_id = 1;
+		$translation_id = 2;
+		$trid = 12345;
+
+		// Insert the test data into the db.
+		$table_name = MyStyle_Wpml::get_instance()->get_translations_table_name();
+		$format = array( '%d', '%s','%d', '%d', '%s', '%s' );
+		$parent_row = array(
+					'translation_id'       => 100,
+					'element_type'         => 'post_page',
+					'element_id'           => $parent_id,
+					'trid'                 => $trid,
+					'language_code'        => 'en',
+					'source_language_code' => null,
+				);
+		$wpdb->insert( $table_name, $parent_row, $format );
+		$translation_row = array(
+					'translation_id'       => 101,
+					'element_type'         => 'post_page',
+					'element_id'           => $translation_id,
+					'trid'                 => $trid,
+					'language_code'        => 'no',
+					'source_language_code' => 'en',
+				);
+		$wpdb->insert( $table_name, $translation_row, $format );
+
+
+		// Call the method.
+		$ret = MyStyle_Wpml::get_instance()->is_translation_of_page( $parent_id, $translation_id );
+
+		// Assert that the method returns true as expected.
+		$this->assertTrue( $ret );
+	}
+
+	/**
+	 * Test that the is_translation_of_page function returns false when the WPML
+	 * plugin is not installed.
+	 *
+	 * @global $wpdb;
+	 */
+	public function test_is_translation_of_page_returns_false_when_wpml_not_installed() {
+		global $wpdb;
+
+		// Set up the test data.
+		$parent_id = 1;
+		$translation_id = 2;
+
+		// Drop the WPML table (added in the setUp method above).
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . MyStyle_Wpml::get_instance()->get_translations_table_name() );
+
+		// Call the method.
+		$ret = MyStyle_Wpml::get_instance()->is_translation_of_page( $parent_id, $translation_id );
+
+		// Assert that the method returned false as expected.
+		$this->assertFalse( $ret );
+	}
+
+	/**
+	 * Private helper method that sets up the database tables which to test
+	 * against.
+	 *
+	 * @global $wpdb;
+	 */
+	public function create_tables() {
+		global $wpdb;
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+		dbDelta( $this->get_schema() );
+	}
+
+	/**
+	 * Private helper method that gets the SQL schema for creating the test
+	 * database table.
+	 *
+	 * @global wpdb $wpdb
+	 * @return string Returns a string containing SQL schema for creating the
+	 * table.
+	 */
+	private function get_schema() {
+
+		$table_name = MyStyle_Wpml::get_instance()->get_translations_table_name();
+
+		$schema = "
+			CREATE TABLE {$table_name} (
+			translation_id bigint(20) NOT NULL,
+			element_type varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'post_post',
+			element_id bigint(20) DEFAULT NULL,
+			trid bigint(20) NOT NULL,
+			language_code varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
+			source_language_code varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+            )";
+
+		return $schema;
 	}
 }
