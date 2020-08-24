6e74fc2f61e77f9ff34054ed6cca7055bf3d52ed
Witty Apps <developer@wittyapps.com>
2020-08-24 14:04:26 -0700

    Change to new version number

--- mystyle.php
+++ mystyle.php
@@ -92,7 +92,7 @@ if ( ! class_exists( 'MyStyle' ) ) :
 				define( 'MYSTYLE_SERVER', 'http://api.ogmystyle.com/' );
 			}
 			if ( ! defined( 'MYSTYLE_VERSION' ) ) {
-				define( 'MYSTYLE_VERSION', '3.13.8' );
+				define( 'MYSTYLE_VERSION', '3.13.9' );
 			}
 			if ( ! defined( 'MYSTYLE_TEMPLATE_DEBUG_MODE' ) ) {
 				define( 'MYSTYLE_TEMPLATE_DEBUG_MODE', false );
