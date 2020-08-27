48b261f35c2d409ca3652d9c027ab0881a89ecf6
Witty Apps <developer@wittyapps.com>
2020-08-27 15:16:58 -0700

    Updated the version number to 3.13.10

--- mystyle.php
+++ mystyle.php
@@ -3,7 +3,7 @@
  * Plugin Name: MyStyle
  * Plugin URI: http://www.mystyleplatform.com
  * Description: The MyStyle Custom Product Designer is a simple plugin that allows your customers to customize products in WooCommerce.
- * Version: 3.13.9
+ * Version: 3.13.10
  * WC requires at least: 2.2.0
  * WC tested up to: 4.2.2
  * Author: mystyleplatform
